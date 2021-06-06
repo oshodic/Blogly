741dcf31e46e2cd47c26ec9e3912e861903b7a18
Christa O <68796545+oshodic@users.noreply.github.com>
2021-06-06 19:46:54 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -15,6 +15,67 @@ db.create_all()
 def homepage():
   """shows homepage w/ list of all users"""
 
-  users = User.query.all()
+  users = User.query.order_by(User.last_name, User.first_name).all()
 
-  return render_template(base.html, users=users)
+  return render_template("base.html", users=users)
+
+@app.route("/user_form", methods=["GET"])
+def user_form():
+  """show form to add new user"""
+
+  return render_template("user_form.html")
+
+@app.route("/user_form", methods=["POST"])
+def add_user():
+  """handle user form"""
+
+  first_name = request.form['first_name']
+  last_name = request.form['last name']
+  img_url = request.form['profile pic']
+
+  user = User(first_name=first_name, last_name=last_name, img_url=img_url)
+  db.session.add(user)
+  db.session.commit()
+
+  return redirect("/")
+
+@app.route("/users/<int:user_id>")
+def show_user(user_id):
+  """shows user profile"""
+  
+  user = User.query.get_or_404(user_id)
+  return render_template('/show_user.html', user=user)
+
+@app.route("/users/<int:user_id>/edit", methods=["POST"])
+def edit_user(user_id):
+  """handles user info edit"""
+
+  user = User.query.get_or_404(user_id)
+  user.first_name = request.form['first_name']
+  user.last_name = request.form['last name']
+  user.img_url = request.form['profile pic']
+
+  db.session.add(user)
+  db.session.commit()
+
+  return redirect("/")
+
+@app.route("/users/<int:user_id>/edit", methods=["GET"])
+def edit_user_get(user_id):
+  """handles user info edit"""
+
+  user = User.query.get_or_404(user_id)
+
+  return render_template("edit_user.html", user=user)
+
+@app.route("/users/<int:user_id>/delete")
+def delete_user(user_id):
+  """delete user profile"""
+
+  user = User.query.get_or_404(user_id)
+  db.session.delete(user)
+  db.session.commit()
+
+  return redirect("/")
+
+
--- models.py
+++ models.py
@@ -20,7 +20,13 @@ class User(db.Model):
   id = db.Column(db.Integer, primary_key=True, autoincrement=True)
   first_name = db.Column(db.String(50), nullable=False)
   last_name = db.Column(db.String(50), nullable=False)                
-  img_url = db.Column(db.Text, default=DEFAULT_IMAGE_URL)
+  img_url = db.Column(db.Text, default=DEFAULT_IMAGE_URL, nullable=False)
+
+  def __repr__(self):
+    """"show info about user"""
+    u = self
+    return f"User {u.id} {u.first_name} {u.last_name} {u.img_url}"
+
 
 def connect_db(app):
   db.app = app
